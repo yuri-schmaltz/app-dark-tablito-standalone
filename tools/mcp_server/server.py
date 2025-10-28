2cc5bd1ae202a5d68e51b6acd58722d993371b01
Mister Schmaltz <63480675+yuri-schmaltz@users.noreply.github.com>
2025-10-28 15:49:47 -0300

    Add CORS headers to JSON responses

--- tools/mcp_server/server.py
+++ tools/mcp_server/server.py
@@ -139,6 +139,9 @@ class MCPRequestHandler(BaseHTTPRequestHandler):
         self.send_response(status)
         self.send_header("Content-Type", "application/json")
         self.send_header("Content-Length", str(len(body)))
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+        self.send_header("Access-Control-Allow-Headers", "Content-Type")
         self.end_headers()
         self.wfile.write(body)
 
